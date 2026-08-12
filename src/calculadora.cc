acd8c39800ceacd09f0a5319f8a8b6b9b3a7a22e
Martin <martinmolinapavon1970@gmail.com>
2026-08-12 17:43:07 +0200

    Cuerpo de las funciones

--- src/calculadora.cc
+++ src/calculadora.cc
@@ -1,3 +1,34 @@
 #include "../include/calculadora.hh"
 
+#include <iostream>
+#include <cmath>
+
 using namespace std;
+
+
+void sumar(long long num1, long long num2) {
+    cout << "El resultado de sumar " << num1 << " + " << num2 << " es " << num1 + num2 << endl;
+
+    return;
+}
+
+
+void restar(long long num1, long long num2) {
+    cout << "El resultado de restar " << num1 << " - " << num2 << " es " << num1 - num2 << endl;
+
+    return;
+}
+
+
+void multiplicar(long long num1, long long num2) {
+    cout << "El resultado de multiplicar " << num1 << " * " << num2 << " es " << num1 * num2 << endl;
+
+    return;
+}
+
+
+void dividir(long long num1, long long num2) {
+    cout << "El resultado de dividir " << num1 << " / " << num2 << " es " << num1 / num2 << ". El modulo es " << num1 % num2 << endl;
+
+    return;
+}
